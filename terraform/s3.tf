452d9301609d65cea5291610d6e87710da51e296
PKDinesh03 <lkgboys@gmail.com>
2026-02-28 11:50:16 +0530

    Fix Terraform provider version and syntax errors

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,3 @@
 resource "aws_s3_bucket" "uploads" {
-bucket = "serverless-${var.environment}-uploads-${random_id.suffix.hex}"}
-
-resource "random_id" "suffix" {
-  byte_length = 4
+  bucket = "serverless-${var.environment}-uploads-${random_id.suffix.hex}"
 }
